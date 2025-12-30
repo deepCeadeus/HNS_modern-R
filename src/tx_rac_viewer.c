c0297d252feaf33c4c8e496055d8a04f480e233a
resetes12 <diploporigon@gmail.com>
2025-12-30 10:00:09 +0100

    Updated info

--- src/tx_rac_viewer.c
+++ src/tx_rac_viewer.c
@@ -1178,7 +1178,7 @@ static void Viewer_RedrawList(void)
 
 void Task_ChallengeViewer(u8 taskId)
 {
-    gMain.savedCallback = CB2_ReturnToFieldContinueScript; // return straight to field
+    gMain.savedCallback = CB2_ReturnToFieldContinueScript; // return to continue script, prevents player moving
     SetMainCallback2(CB2_InitChallengeViewer);
     DestroyTask(taskId);
 }
