2b31f3e37a8553a46286aac59fcd448b9e1a4200
resetes12 <diploporigon@gmail.com>
2026-08-02 12:02:52 +0200

    Fixed more compilation issues

--- src/main_menu.c
+++ src/main_menu.c
@@ -2065,7 +2065,7 @@ static void SpriteCB_MovePlayerDownWhileShrinking(struct Sprite *sprite)
 
 static u8 NewGameBirchSpeech_CreateLotadSprite(u8 x, u8 y)
 {
-    return CreateMonPicSprite_Affine(SPECIES_WOOPER, SHINY_ODDS << gSaveBlock1Ptr->tx_Features_ShinyChance, 0, MON_PIC_AFFINE_FRONT, x, y, 14, TAG_NONE);
+    return CreateMonPicSprite_Affine(SPECIES_WOOPER, Random32(), Random32(), MON_PIC_AFFINE_FRONT, x, y, 14, TAG_NONE);
 }
 
 static void AddBirchSpeechObjects(u8 taskId)
--- src/scrcmd.c
+++ src/scrcmd.c
@@ -66,6 +66,7 @@
 #include "list_menu.h"
 #include "malloc.h"
 #include "starter_choose.h"
+#include "new_game.h"
 
 //HnS For headbutt
 // From party_menu.c (that TU owns the tutor data)
@@ -1653,7 +1654,6 @@ bool8 ScrCmd_showmonpic(struct ScriptContext *ctx)
     // If we have not gotten a pokemon yet, assume this is starter preview
     if (!FlagGet(FLAG_SYS_POKEMON_GET))
     {
-        u32 shinyChance = SHINY_ODDS << gSaveBlock1Ptr->tx_Features_ShinyChance;
         u8 starter = VarGet(VAR_STARTER_MON);
 
         u32 flagTemp;
@@ -1681,7 +1681,7 @@ bool8 ScrCmd_showmonpic(struct ScriptContext *ctx)
         if (!FlagGet(flagTemp))
         {
             u32 fakePid = Random32();
-            if (GET_SHINY_VALUE(GetTrainerId(), fakePid) < shinyChance)
+            if (IsShinyOtIdPersonality(GetTrainerId(gSaveBlock2Ptr->playerTrainerId), fakePid))
                 FlagSet(flagShinyStarter);
 
             FlagSet(flagTemp);
