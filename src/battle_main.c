edddfe0705101b2aabb91979ba37a58939f60d3c
deepCeadeus <r27094257@gmail.com>
2026-08-13 17:10:57 -0700

    port bugfix

--- src/battle_main.c
+++ src/battle_main.c
@@ -4411,7 +4411,7 @@ static void BattleIntroSafariQuickRun(void)
             if ((JOY_HELD(R_BUTTON)) && (JOY_HELD(L_BUTTON)))
             {
                 PlaySE(SE_FLEE);
-                gBattlerAttacker = gBattlerByTurnOrder[gCurrentTurnActionNumber];
+                //gBattlerAttacker = gBattlerByTurnOrder[gCurrentTurnActionNumber];
                 gBattleOutcome = B_OUTCOME_RAN;
                 gBattleMainFunc = HandleEndTurn_RanFromBattle;
                 return;
@@ -4435,7 +4435,7 @@ static void BattleIntroSafariQuickRun(void)
 
                 sSafariRunHoldCounter = 0;
                 PlaySE(SE_FLEE);
-                gBattlerAttacker = gBattlerByTurnOrder[gCurrentTurnActionNumber];
+                //gBattlerAttacker = gBattlerByTurnOrder[gCurrentTurnActionNumber];
                 gBattleOutcome = B_OUTCOME_RAN;
                 gBattleMainFunc = HandleEndTurn_RanFromBattle;
                 return;
